463f221950561358c1ebbabf4950c6d870aa2376
SANDIP DAS <108420428+sandip2233@users.noreply.github.com>
2023-05-24 19:51:07 +0530

    Update SubmittedForms.js

--- client/src/SubmittedForms.js
+++ client/src/SubmittedForms.js
@@ -7,7 +7,7 @@ const SubmittedForms = () => {
   useEffect(() => {
 
     const fetchForms = async () => {
-      const response = await fetch('https://user-form-glbj.onrender.com/submitted-forms');
+      const response = await fetch('https://server-almd.onrender.com/submitted-forms');
       const data = await response.json();
       setForms(data);
     };
